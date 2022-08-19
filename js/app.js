7ac18b3cf6518a5ee1a2785d88561c83609a67fb
Dmitry Pavlov <pavlov.d60@gmail.com>
2022-08-19 20:06:47 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,9 +2,7 @@ function draw() {
   var canvas = document.getElementById('canvas');
   if (canvas.getContext) {
     var context = canvas.getContext('2d');
-        context.beginPath();
-        context.rect(0, 0, 50, 50);
-        context.stroke();
+      context.rect(0, 0, 50, 50);
   }
 }
 
