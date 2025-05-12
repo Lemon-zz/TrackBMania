882e8f60dd0effe1b715e433e541282d56c909e9
Dmitrii Pavlov <mr-pavlov@yandex-team.ru>
2025-05-12 17:48:29 +0300

    Added files

--- js/app.js
+++
@@ -1,8 +0,0 @@
-function draw() {
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var context = canvas.getContext('2d');
-      context.rect(0, 0, 50, 50);
-  }
-}
-
